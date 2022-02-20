85ca4e0ac165dc5faaa075706557bc263c000ee4
Alex <100072255+lexavb76@users.noreply.github.com>
2022-02-20 19:08:04 +0500

    Add scripts to maintain neovim and neovim-qt
    Place the corresponding scripts into the respective git repository you have assumingly cloned.
    !!! Use these scripts unless you use you package manager for neovim

---
+++ start-nvim.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+com=$1
+cur_ver=$(git log | head -3) # | awk '{print $2}')
+
+function install() {
+    echo '****************'
+    git branch -a
+    git tag --column
+    #git log | egrep 'NVIM v[[:digit:]]?\.[[:digit:]]?\.[[:digit:]]$' | head -n 5 | awk '{print $2}'
+    echo '****************'
+    echo "Your current revision is: $cur_ver"
+    echo '****************'
+    read -p "Choose release: " rev
+    [ -n "$rev" ] && git checkout $rev
+    read -p "Reinstalling your nvim. Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit_ 1
+    uninstall
+    #make CMAKE_BUILD_TYPE=RelWithDebInfo USE_BUNDLED=OFF && \
+    make CMAKE_BUILD_TYPE=RelWithDebInfo && \
+    sudo make install
+    if [ -d /usr/local/bin/nvim ]; then
+        sudo mv /usr/local/bin/nvim /usr/local/bin/nvim.bak
+        sudo mv /usr/local/bin/nvim.bak/nvim /usr/local/bin/
+        sudo rm -rf /usr/local/bin/nvim.bak
+    fi
+}
+
+function uninstall() {
+    sudo rm  -v /usr/local/bin/nvim
+    sudo rm -rv /usr/local/share/nvim/
+}
+
+function exit_() {
+    echo "---------------------------------------"
+    new_ver=$(git log | head -3) # | awk '{print $2}')
+    echo "Your old revision was: $cur_ver"
+    echo "Your new current revision is: $new_ver"
+    echo "---------------------------------------"
+    nvim --version
+    echo "---------------------------------------"
+    echo "Uninstall: ./start-nvim.sh uninstall"
+    exit $1
+}
+
+git status
+git pull
+if [ "$com" = "uninstall" ]; then
+    uninstall
+else
+    install
+fi
+
